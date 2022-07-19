8a15200d83c25cbe9ec6354b97ab6bd726490043
Adam <adam.wallace@communities.gov.uk>
2022-07-19 11:55:07 +0100

    add sql env

--- config/envs/test.py
+++ config/envs/test.py
@@ -2,14 +2,19 @@
 import logging
 from os import environ
 
-from config.envs.default import DefaultConfig as Config
+from config.envs.default import DefaultConfig
 from fsd_utils import configclass
 
 
 @configclass
-class TestConfig(Config):
+class TestConfig(DefaultConfig):
 
     SECRET_KEY = environ.get("SECRET_KEY", "test")
 
     # Logging
     FSD_LOG_LEVEL = logging.DEBUG
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL").replace(
+        "postgres://", "postgresql://"
+    )
